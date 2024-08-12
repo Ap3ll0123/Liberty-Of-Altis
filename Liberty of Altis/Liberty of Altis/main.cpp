07e08eaa06148711b40293ac6a93f153c8171d9e
HeinLinnHtet <69047577+HeinLinnHtet@users.noreply.github.com>
2024-08-12 16:17:42 +0800

    File Creation + base map

---
+++ Liberty of Altis/Liberty of Altis/main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <windows.h>
+using namespace std;
+
+void gotoxy(int x, int y) {
+	COORD srcn;
+	HANDLE userhandle = GetStdHandle(STD_OUTPUT_HANDLE);
+	srcn.X = x; srcn.Y = y;
+	SetConsoleCursorPosition(userhandle, srcn);
+}
+
+int main()
+{
+	for (int i = 0; i < 25; i++)
+	{
+		for (int j = 0; j < 18; j++)
+		{
+			gotoxy(i * 2, j);
+			cout << "*";
+		}
+	}
+}
