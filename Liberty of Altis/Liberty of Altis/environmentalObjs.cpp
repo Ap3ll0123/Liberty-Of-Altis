bbd8cc33c6ecf3d60a80dacf39efe1947ff4c257
HeinLinnHtet <69047577+HeinLinnHtet@users.noreply.github.com>
2024-08-13 13:47:07 +0800

    forgot 1 class

--- Liberty of Altis/Liberty of Altis/environmentalObjs.cpp
+++ Liberty of Altis/Liberty of Altis/environmentalObjs.cpp
@@ -1,16 +1,35 @@
 #include "environmentalObjs.h"
 #include <iostream>
 
+environmentalObjs::environmentalObjs(char sym, int xp, int yp)
+{
+	objsymbol = sym;
+	objPos.SetX(xp);
+	objPos.SetY(yp);
+}
+
+environmentalObjs::environmentalObjs()
+{
+
+}
+
 void environmentalObjs::drawEnvObj()
 {
 	std::cout << objsymbol;
 }
 
-void environmentalObjs::setsymbol(char i)
-{
-	objsymbol = i;
-}
 char environmentalObjs::getsymbol()
 {
 	return objsymbol;
 }
+
+int environmentalObjs::getx()
+{
+	return objPos.GetX();
+}
+
+int environmentalObjs::gety()
+{
+	return objPos.GetY();
+}
+
--- Liberty of Altis/Liberty of Altis/environmentalObjs.h
+++ Liberty of Altis/Liberty of Altis/environmentalObjs.h
@@ -1,11 +1,18 @@
 #pragma once
+#include "Position.h"
 class environmentalObjs
 {
 private:
 	char objsymbol;
 public :
-	void setsymbol(char i);
+	Position objPos;
+
+	environmentalObjs(char sym, int xp, int yp);
+	environmentalObjs();
+
 	char getsymbol();
+	int getx();
+	int gety();
 
 	void drawEnvObj();
 };
