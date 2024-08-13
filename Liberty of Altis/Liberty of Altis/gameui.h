fa3cff194ebd13a8693b1207a16f010612559b95
speedy81523 <85233801+speedy81523@users.noreply.github.com>
2024-08-13 15:52:30 +0800

    Update gameui.h

--- Liberty of Altis/Liberty of Altis/gameui.h
+++ Liberty of Altis/Liberty of Altis/gameui.h
@@ -8,7 +8,9 @@ public:
     gameui();
     void cutscene1();
     void stats();
+    int choice;
     void gamestart(gameui& player);
     int getstats(int H, int a, int r, int c, int s);
+    void turtorial();
 };
 
