c9f82cc8ca9a8a607e757918ca74b8b83f14c9bd
AlexPastGrey <140066150+AlexPastGrey@users.noreply.github.com>
2024-08-13 11:49:02 +0800

    Update AlliedTroops.cpp

--- Liberty of Altis/Liberty of Altis/AlliedTroops.cpp
+++ Liberty of Altis/Liberty of Altis/AlliedTroops.cpp
@@ -1,6 +1,62 @@
 #include "AlliedTroops.h"
 #include <iostream>
 
-void Movement(){
-  
+void AlliedTroops::Movement(char direction)
+{
+	char dirinput = toupper(direction);
+
+	if (dirinput == 'W') {
+		if (GetY() == 0) {
+			SetY(GetY());
+			std::cout << GetY() << std::endl;
+		}
+		else {
+			SetY(GetY() - 1);
+			std::cout << GetY() << std::endl;
+		}
+	}
+	else if (dirinput == 'S') {
+		if (GetY() == 17) {
+			SetY(GetY());
+			std::cout << GetY() << std::endl;
+		}
+		else {
+			SetY(GetY() + 1);
+			std::cout << GetY() << std::endl;
+		}
+	}
+	else if (dirinput == 'A') {
+		if (GetX() == 0) {
+			SetX(GetX());
+			std::cout << GetX() << std::endl;
+		}
+		else {
+			SetX(GetX() - 1);
+			std::cout << GetX() << std::endl;
+		}
+	}
+	else if (dirinput == 'D') {
+		if (GetX() == 24) {
+			SetX(GetX());
+			std::cout << GetX() << std::endl;
+		}
+		else {
+			SetX(GetX() + 1);
+			std::cout << GetX() << std::endl;
+		}
+	}
+
+}
+
+void AlliedTroops::Attacking()
+{
+
+}
+
+bool AlliedTroops::collisioncheck(Entity& other)
+{
+	// ur pos = other pos - 1
+	if (GetX() == other.GetX()) {
+		return true;
+	}
 }
