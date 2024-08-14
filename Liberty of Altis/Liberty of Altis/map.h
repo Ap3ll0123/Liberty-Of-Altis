4aa292d3bb96e6bfce0792528ce8871fd79ff734
NK178 <ayhngern@gmail.com>
2024-08-14 15:23:14 +0800

    Update map.h

--- Liberty of Altis/Liberty of Altis/map.h
+++ Liberty of Altis/Liberty of Altis/map.h
@@ -1,5 +1,22 @@
 #pragma once
 #include "environmentalObjs.h"
+#include "ARiflesman.h"
+#include "AArtillery.h"
+#include "AGrenadier.h"
+#include "AMachinegunner.h"
+#include "ATank.h"
+#include "AMotar.h"
+#include "AIFV.h"
+
+#include "ERiflesman.h"
+#include "EArtillery.h"
+#include "EGrenadier.h"
+#include "EMachinegunner.h"
+#include "ETank.h"
+#include "EMotar.h"
+#include "EIFV.h"
+
+
 
 class map :
 	public environmentalObjs
