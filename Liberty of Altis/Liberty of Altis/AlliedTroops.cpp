2de9903852d3526948db0b4a08aa06cbc3be2cc0
NK178 <ayhngern@gmail.com>
2024-08-14 15:43:25 +0800

    UPdated allied troops

--- Liberty of Altis/Liberty of Altis/AlliedTroops.cpp
+++ Liberty of Altis/Liberty of Altis/AlliedTroops.cpp
@@ -14,47 +14,44 @@ void AlliedTroops::Movement(char direction)
 {
 	char dirinput = toupper(direction);
 
+
+
 	if (dirinput == 'W') {
 		if (PosXY.GetX() == 0) {
-			PosXY.SetY(PosXY.GetY());
-			std::cout << PosXY.GetY() << std::endl;
+			//std::cout << PosXY.GetY() << std::endl;
 		}
 		else {
 			PosXY.SetY(PosXY.GetY() - 1);
-			std::cout << PosXY.GetY() << std::endl;
+			//std::cout << PosXY.GetY() << std::endl;
 		}
 	}
 	else if (dirinput == 'S') {
 		if (PosXY.GetY() == 17) {
-			PosXY.SetY(PosXY.GetY());
-			std::cout << PosXY.GetY() << std::endl;
+			//std::cout << PosXY.GetY() << std::endl;
 		}
 		else {
 			PosXY.SetY(PosXY.GetY() + 1);
-			std::cout << PosXY.GetY() << std::endl;
+			//std::cout << PosXY.GetY() << std::endl;
 		}
 	}
 	else if (dirinput == 'A') {
 		if (PosXY.GetX() == 0) {
-			PosXY.SetX(PosXY.GetX());
-			std::cout << PosXY.GetX() << std::endl;
+			//std::cout << PosXY.GetX() << std::endl;
 		}
 		else {
 			PosXY.SetX(PosXY.GetX() - 1);
-			std::cout << PosXY.GetX() << std::endl;
+			//std::cout << PosXY.GetX() << std::endl;
 		}
 	}
 	else if (dirinput == 'D') {
 		if (PosXY.GetX() == 50) {
-			PosXY.SetX(PosXY.GetX());
-			std::cout << PosXY.GetX() << std::endl;
+			//std::cout << PosXY.GetX() << std::endl;
 		}
 		else {
 			PosXY.SetX(PosXY.GetX() + 1);
-			std::cout << PosXY.GetX() << std::endl;
+			//std::cout << PosXY.GetX() << std::endl;
 		}
 	}
-	std::cout << "ALlyMOve";
 }
 
 void AlliedTroops::Attacking(char direction)
